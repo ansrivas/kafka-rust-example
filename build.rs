62738aba36221c24bffba14ccb98ee14dc1d6f2f
Ankur Srivastava <best.ankur@gmail.com>
2021-08-01 15:47:51 +0200

    deps: bump up prost to 0.8 and regenerate files

--- build.rs
+++ build.rs
@@ -4,16 +4,12 @@ fn main() {
 	// ***** NOTE ******
 	// Uncomment this so as to copy the new file
 
-	// use std::{
-	// 	env,
+	//use std::{
+	// 	env, fs,
 	// 	path::{Path, PathBuf},
 	// };
 	// let dst = PathBuf::from(env::var_os("OUT_DIR").unwrap());
 	// let tmp_generated_message = dst.join("messages.rs");
 	// let output_file = PathBuf::from("src/generated/messages.rs");
-	// fs::copy(
-	//    tmp_generated_message,
-	//     output_file,
-	// )
-	// .unwrap();
+	// fs::copy(tmp_generated_message, output_file).unwrap();
 }
